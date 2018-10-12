dadc835fb07fd86dbf74876c4da145d11271fb2b
Sam Wong <sam_wong156@hotmail.com>
2018-10-11 20:17:52 -0400

    General: fixed Tile value enum

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -4,7 +4,15 @@ public class Tile {
     }
     private static enum values{
         ONE(1),TWO(2),THREE(3),FOUR(4),FIVE(5),SIX(6),SEVEN(7),EIGHT(8),NINE(9),TEN(10),ELEVEN(11),TWELVE(12),THIRTEEN(13);
-        private final in val;
+        private final int val;
+
+        values(int val){
+            this.val = val;
+        }
+
+        public int getVal(){
+            return this.val;
+        }
     }
     private String col;
     private String val;
