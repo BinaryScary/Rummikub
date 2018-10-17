15667b2bfca3aa7a67d2fa37a023e286c071cf54
KartikVashisth97 <kartikvashisth675@gmail.com>
2018-10-17 18:54:35 -0400

    init: Game class created, Meld class methods added

--- src/main/java/Meld.java
+++ src/main/java/Meld.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class Meld {	
+		
+		private ArrayList<Tile> melded_tiles = new ArrayList<Tile>(); 
+		
+		public Meld (ArrayList<Tile> c) {
+			addTiles(c);  
+		}
+		
+		public void addTiles(ArrayList<Tile> c) {
+			melded_tiles.addAll(c); 
+		}
+		
+		public void addTile(Tile c) {
+			melded_tiles.add(c); 
+		}
+		
+		public void removeTile(Tile c) {
+			melded_tiles.remove(c); 
+		}
 
-public class Meld {
-
+		public void removeTiles (ArrayList<Tile> c) {
+			melded_tiles.removeAll(c); 
+		}
+		
 }
+
