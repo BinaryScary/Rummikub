4d0d8a4fbca23abed7edc571932ecadf25a5f198
Sam Wong <sam_wong156@hotmail.com>
2018-10-17 16:55:21 -0400

    General: fixed Tile constructors

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,20 +1,34 @@
+
 public class Tile {
-    private static enum colours{
+    private static enum colour{
         RED,GREEN,BLUE,ORANGE;
     }
-    private static enum values{
+    private static enum value{
         ONE(1),TWO(2),THREE(3),FOUR(4),FIVE(5),SIX(6),SEVEN(7),EIGHT(8),NINE(9),TEN(10),ELEVEN(11),TWELVE(12),THIRTEEN(13);
         private final int val;
 
-        values(int val){
+        value(int val){
             this.val = val;
         }
 
         public int getVal(){
             return this.val;
         }
-    }
-    private String col;
-    private String val;
 
+        public int getCol(){
+            return this.val;
+        }
+    }
+    private colour col;
+    private value val;
+
+    public Tile(colour c, value v){
+        col = c;
+        val = v;
+    }
+
+    public Tile(){
+        col = null;
+        val = null;
+    }
 }
