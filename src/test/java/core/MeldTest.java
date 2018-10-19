eb555e6222620e2d14265a3ecaecae617409f01d
Sam Wong <sam_wong156@hotmail.com>
2018-10-19 15:11:56 -0400

    Test: Added MeldTest file with appropriate tests

--- src/test/java/core/MeldTest.java
+++ src/test/java/core/MeldTest.java
@@ -1,5 +1,32 @@
 package core;
+import junit.framework.TestCase;
 
-public class MeldTest {
+public class MeldTest extends TestCase {
+	public void testMeldInit() {
+		Tile[] tArr = {new Tile(Tile.colour.RED, Tile.value.ONE),new Tile(Tile.colour.BLUE, Tile.value.TWO)};
+		Meld m1 = new Meld();
+		Meld m2 = new Meld(tArr);
 
+		assertNotNull(m1);
+		assertNotNull(m2);
+	}
+	public void testMeldString() {
+		Tile[] tArr = {new Tile(Tile.colour.RED, Tile.value.ONE),new Tile(Tile.colour.BLUE, Tile.value.TWO)};
+		Meld m1 = new Meld(tArr);
+		assertEquals("RED ONE BLUE TWO",m1.toString());
+	}
+	public void testMeldEquals() {
+		Tile[] tArr1 = {new Tile(Tile.colour.RED, Tile.value.ONE),new Tile(Tile.colour.BLUE, Tile.value.TWO)};
+		Tile[] tArr2 = {new Tile(Tile.colour.RED, Tile.value.ONE),new Tile(Tile.colour.BLUE, Tile.value.TWO)};
+		Meld m1 = new Meld(tArr1);
+		Meld m2 = new Meld(tArr2);
+		assertEquals(m1, m2);
+	}
+	
+	public void testMeldTotal() {
+		Tile[] tArr1 = {new Tile(Tile.colour.RED, Tile.value.ONE),new Tile(Tile.colour.BLUE, Tile.value.TWO)};
+		Meld m1 = new Meld(tArr1);
+		
+		assertEquals(3, m1.totalMeld());
+	}
 }
