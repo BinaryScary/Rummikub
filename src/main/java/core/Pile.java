4e990de10efa7aec6fa525ae4fc4ee084c3b1a59
AbdouSarr <sarr.abdoullahi@gmail.com>
2018-10-23 22:25:19 -0400

    Refactor Pile Class

--- src/main/java/core/Pile.java
+++ src/main/java/core/Pile.java
@@ -1,5 +1,38 @@
 package core;
 
-public class Pile {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Pile {
+	
+	private ArrayList<Tile> pile;
+	
+	public Pile() {
+		pile = new ArrayList<Tile>();
+		formPile(); formPile();
+	}
+	
+	private void formPile() {
+		for(int c = 0; c < 4; c++) {
+			for(int v = 0; v < 13; v++) {
+				Tile tile = new Tile(Tile.colour.values()[c], Tile.value.values()[v]);
+				pile.add(tile); 
+			}
+		}
+	}
+	
+	public int numTiles() { return pile.size(); }
+	
+	public Tile deal() {
+		if(pile.size() > 0) {
+			return pile.remove(0);
+		} else {
+			return null;
+		}
+	}
+	
+	public boolean scramble(){
+		Collections.shuffle(pile);
+		return true;
+	}
 }
