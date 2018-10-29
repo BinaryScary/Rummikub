a1e2ac9b068d9c4c5eaeb68824fb5793f2b1ab30
KartikVashisth97 <kartikvashisth675@gmail.com>
2018-10-29 15:50:28 -0400

    init Player class refactored

--- src/main/java/core/Player.java
+++ src/main/java/core/Player.java
@@ -3,8 +3,24 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class Player implements Observer{
+	
+	private String name;
+	private Hand hand; 
+	private Table table; // player can see table itself
+	private Pile pile; // player can see deck itself
+	
+	
+	public Player (String k, int id){ //should have player strategy
+		
+		name = k;
+		hand = new Hand(name+"'s Hand"); 
+		int playerID = id; 
+    	//should contain player strategy
+    	pile = new Pile();
+    	table = new Table();
 
-	   public void update(Observable obs, Object x) {
+	}
+	public void update(Observable obs, Object x) {
 		   
-	   }
+	}
 }
