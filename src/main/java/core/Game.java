d9dc3f65f452c1cc66672c7d27f5ba9db20550b8
Sam Wong <sam_wong156@hotmail.com>
2018-11-02 15:19:28 -0400

    Core: added getters/setters and Observer implementation

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -5,24 +5,27 @@ import java.util.Observable;
 @SuppressWarnings("deprecation")
 public class Game extends Observable{ 
 	private Pile pile;
-	private Hand h1;
-	private Hand h2;
-	private Hand h3;
-	private Hand h4;
 	private Table table;
 	private CLI cli;
+	private Player human;
+	private Player p1;
+	private Player p2;
+	private Player p3;
 	
 	public void init() {
 		pile = new Pile();
-//		h1 = new Hand("human");
-//		h2 = new Hand("p1");
-//		h3 = new Hand("p2");
-//		h4 = new Hand("p3");
 		table = new Table();
 		cli = new CLI();
-		
+		Player human = new Player("human", 0);
+		Player p1 = new Player("p1", 1);
+		Player p2 = new Player("p2", 2);
+		Player p3 = new Player("p3", 3);
 		pile.scramble();
 		
+		this.addObserver(human);
+		this.addObserver(p1);
+		this.addObserver(p2);
+		this.addObserver(p3);
 	}
 	
 	//init(file)
@@ -33,6 +36,54 @@ public class Game extends Observable{
 	
 	private void gameLogic() {
 		
+		
+		
+		
+		
+	}
+	
+	private void broadcast() {
+		notifyObservers();
+	}
+
+	public Table getTable() {
+		return table;
+	}
+
+	public void setTable(Table table) {
+		this.table = table;
+	}
+
+	public Player getHuman() {
+		return human;
+	}
+
+	public void setHuman(Player human) {
+		this.human = human;
+	}
+
+	public Player getP1() {
+		return p1;
+	}
+
+	public void setP1(Player p1) {
+		this.p1 = p1;
+	}
+
+	public Player getP2() {
+		return p2;
+	}
+
+	public void setP2(Player p2) {
+		this.p2 = p2;
+	}
+
+	public Player getP3() {
+		return p3;
+	}
+
+	public void setP3(Player p3) {
+		this.p3 = p3;
 	}
 	
 	
--- src/main/java/core/GameStart.java
+++ src/main/java/core/GameStart.java
@@ -5,7 +5,7 @@ public class GameStart {
 	public static void main(String[] args) {
 		Game g = new Game();
 		g.init();
-
+		System.out.println("done");
 	}
 
 }
