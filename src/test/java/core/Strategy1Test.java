004fd80cb9d113a25aef4835928f5c35ab7cdded
Sam Wong <sam_wong156@hotmail.com>
2018-11-05 02:52:42 -0500

    Test: added playTile test

--- src/test/java/core/Strategy1Test.java
+++ src/test/java/core/Strategy1Test.java
@@ -8,10 +8,22 @@ public class Strategy1Test extends TestCase {
 		Hand h = new Hand(tArr);
 		Strategy1 p = new Strategy1(h);
 
-		System.out.println(p.playableSets());
 		assertNotNull(p.playableSets());
 	}
 	
+	public void testPlayTile() {
+		Tile[] tArr1 = {new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.ORANGE, Tile.value.TEN)};
+		Meld m1 = new Meld(tArr1);
+		Meld[] mArr = {m1};
+		Table t1 = new Table(mArr);
+		Tile[] tArr = {new Tile(Tile.colour.GREEN, Tile.value.TEN)};
+		Hand h = new Hand(tArr);
+		Strategy1 p = new Strategy1(h,t1);
+		
+		p.play();
+		assertTrue(p.getHand().isEmpty());
+	}
+
 	public void testPlayMeld() {
 		Tile[] tArr = {new Tile(Tile.colour.BLUE, Tile.value.NINE), new Tile(Tile.colour.GREEN, Tile.value.NINE), new Tile(Tile.colour.RED, Tile.value.NINE)};
 		Meld m = new Meld(new Tile[]{new Tile(Tile.colour.BLUE, Tile.value.NINE), new Tile(Tile.colour.GREEN, Tile.value.NINE), new Tile(Tile.colour.RED, Tile.value.NINE)});
@@ -25,7 +37,6 @@ public class Strategy1Test extends TestCase {
 		Tile[] tArr = {new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.RED, Tile.value.ELEVEN),new Tile(Tile.colour.RED, Tile.value.TWELVE),new Tile(Tile.colour.RED, Tile.value.THIRTEEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TWELVE),new Tile(Tile.colour.ORANGE, Tile.value.ONE),new Tile(Tile.colour.ORANGE, Tile.value.TWO),new Tile(Tile.colour.ORANGE, Tile.value.THREE)};
 		Hand h = new Hand(tArr);
 		Strategy1 p = new Strategy1(h);
-		System.out.println(p.playableRuns());
 		assertNotNull(p.playableRuns());
 	}
 }
