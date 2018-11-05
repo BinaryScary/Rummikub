9b78398503597c8b69ec271c51c145db5d3f2712
Sam Wong <sam_wong156@hotmail.com>
2018-11-05 01:08:11 -0500

    Core: Implemented Strategy1 HasSet

--- src/main/java/core/Strategy1.java
+++ src/main/java/core/Strategy1.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 
 public class Strategy1 extends Player {
 	
+	public Strategy1() {
+		super();
+		name = "A1";
+	}
+	public Strategy1(Hand h) {
+		super(h);
+		name = "A1";
+	}
+
 	private CLI ui;
 
 	@Override
@@ -16,10 +25,27 @@ public class Strategy1 extends Player {
 
 	}
 
-	private Meld[] playableSets() {
+	@SuppressWarnings("unlikely-arg-type")
+	public ArrayList<Meld> playableSets() {
+		ArrayList<Meld> arr = new ArrayList<Meld>();
+		Meld tempMeld;
+		
+		for(Tile t : hand.getTiles()) {
+			tempMeld = new Meld();
+			for(Tile.colour c : Tile.colour.values()) {
+				if(hand.getTiles().indexOf(new Tile(c, t.getValue())) != -1) {
+					tempMeld.add(hand.getTile(hand.getTiles().indexOf(new Tile(c, t.getValue()))));
+				}
+			}
+			if(tempMeld.getMeld().size() >=3 && !arr.contains(tempMeld)) {
+				arr.add(tempMeld);
+			}
+		}
+		
+		return arr;
 	}
-	private Meld[] playableRuns() {
-	}
+//	private Meld[] playableRuns() {
+//	}
 
 	private void playMeld(Meld m) {
 		
--- src/test/java/core/Strategy1Test.java
+++ src/test/java/core/Strategy1Test.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class Strategy1Test extends TestCase {
 	public void testHasSet() {
-		Tile[] tArr = {new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.GREEN, Tile.value.TEN),new Tile(Tile.colour.ORANGE, Tile.value.TEN),new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.GREEN, Tile.value.TEN),new Tile(Tile.colour.ORANGE, Tile.value.TEN)};
+		Tile[] tArr = {new Tile(Tile.colour.BLUE, Tile.value.NINE), new Tile(Tile.colour.GREEN, Tile.value.NINE), new Tile(Tile.colour.RED, Tile.value.NINE), new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.GREEN, Tile.value.TEN),new Tile(Tile.colour.ORANGE, Tile.value.TEN),new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.GREEN, Tile.value.TEN),new Tile(Tile.colour.ORANGE, Tile.value.TEN)};
 		Hand h = new Hand(tArr);
 		Strategy1 p = new Strategy1(h);
 
