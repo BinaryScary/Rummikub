c1c938fe618d4e4c460a5a00945de21904585baa
AbdouSarr <sarr.abdoullahi@gmail.com>
2018-11-05 01:26:54 -0500

    S1: Playable Runs

--- src/test/java/core/Strategy1Test.java
+++ src/test/java/core/Strategy1Test.java
@@ -4,12 +4,19 @@ import junit.framework.TestCase;
 
 public class Strategy1Test extends TestCase {
 	public void testHasSet() {
-		Tile[] tArr = {new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.GREEN, Tile.value.TEN),new Tile(Tile.colour.ORANGE, Tile.value.TEN),new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.GREEN, Tile.value.TEN),new Tile(Tile.colour.ORANGE, Tile.value.TEN)};
+		Tile[] tArr = {new Tile(Tile.colour.BLUE, Tile.value.NINE), new Tile(Tile.colour.GREEN, Tile.value.NINE), new Tile(Tile.colour.RED, Tile.value.NINE), new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.GREEN, Tile.value.TEN),new Tile(Tile.colour.ORANGE, Tile.value.TEN),new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.GREEN, Tile.value.TEN),new Tile(Tile.colour.ORANGE, Tile.value.TEN)};
 		Hand h = new Hand(tArr);
 		Strategy1 p = new Strategy1(h);
 
 		System.out.println(p.playableSets());
 		assertNotNull(p.playableSets());
 	}
-
+	
+	public void testHasRuns() {
+		Tile[] tArr = {new Tile(Tile.colour.RED, Tile.value.TEN),new Tile(Tile.colour.RED, Tile.value.ELEVEN),new Tile(Tile.colour.RED, Tile.value.TWELVE),new Tile(Tile.colour.RED, Tile.value.THIRTEEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TEN),new Tile(Tile.colour.BLUE, Tile.value.TWELVE),new Tile(Tile.colour.ORANGE, Tile.value.ONE),new Tile(Tile.colour.ORANGE, Tile.value.TWO),new Tile(Tile.colour.ORANGE, Tile.value.THREE)};
+		Hand h = new Hand(tArr);
+		Strategy1 p = new Strategy1(h);
+		System.out.println(p.playableRuns());
+		assertNotNull(p.playableRuns());
+	}
 }
